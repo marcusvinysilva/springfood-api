84aeab02b72db32544706d4fbcaf4f9991122bb3
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-10-14 15:49:42 -0300

    feat: init spring project

---
+++ src/test/java/com/spring/springfood/SpringfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
